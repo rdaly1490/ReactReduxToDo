7613a4e766876ac347c2c2d9ca83dfd5169856f5
Rob <rdaly1490@gmail.com>
2017-01-29 20:47:47 -0600

    fake data loading in

---
+++ src/data/mockData.js
@@ -0,0 +1,33 @@
+export default
+{
+	"todos": [
+		{
+			"text": "Hello World1",
+			"completed": false
+		},
+		{
+			"text": "Hello World2",
+			"completed": false
+		},
+		{
+			"text": "Hello World3",
+			"completed": false
+		},
+		{
+			"text": "Hello World4",
+			"completed": false
+		},
+		{
+			"text": "Hello World5",
+			"completed": false
+		},
+		{
+			"text": "Hello World6",
+			"completed": false
+		},
+		{
+			"text": "Hello World7",
+			"completed": false
+		}
+	]
+}
